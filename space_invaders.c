ffe2080aa3e429f2726d6bd61b8370a9b4cad4d9
Gustavo <gustavogomes1108@gmail.com>
2025-07-04 13:37:50 -0300

    .

--- space_invaders.c
+++ space_invaders.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 int main(){
+	int i;
+	int j;
+	
+	for (i = 0; i < 50; i++){
+		printf("-");
+	}
+	
+	printf("\n");
+	
+	for (i = 0; i < 5; i++){
+		printf("|");
+			
+		for (j = 1; j < 49; j++){
+			
+			printf(" ");
+		} 
+		
+		printf("|\n");
+	}
+	
+	for (i = 0; i < 50; i++){
+		printf("-");
+	}
 
 	return 0;
 }
